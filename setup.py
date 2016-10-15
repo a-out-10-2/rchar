e7b31f893192b8f489624015789944b07efcee2c
moea <moe.andrew@acm.org>
2016-10-15 13:41:16 -0500

    Added setup.py .

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rchar',
+    version='1.1',
+    packages=[''],
+    url='https://bitbucket.org/a_out_10_2/rchar',
+    license='GPLv2',
+    author='a_out_10_2',
+    author_email='',
+    description=''
+)
