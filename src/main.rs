e035b970d980ee01aefd2bfdbbd6005797d81467
a_out_10_2 <moe.andrew@gmail.com>
2021-09-29 18:39:28 -0500

    Added feature for randomization by regularish expression.

--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,15 @@ use std::char;
 use std::process;
 
 use argparse::{ArgumentParser, StoreTrue, Store};
+use rand::Rng;
 use rand::seq::IteratorRandom;
+use rand_regex::Regex;
 
 const OP_CHARSCOPE:u8 = 1;
-const OP_ALPHANUM:u8 = 2;
-const OP_PRINTABLE:u8 = 4;
-const OP_ALLASCII:u8 = 8;
+const OP_REGEX:u8 = 2;
+const OP_ALPHANUM:u8 = 4;
+const OP_PRINTABLE:u8 = 8;
+const OP_ALLASCII:u8 = 16;
 
 fn range2charscope(lobound:u32, hibound:u32) -> String
 {
@@ -52,14 +55,26 @@ fn generate_string_from_charscope(rng: &mut impl rand::Rng,
     }
 }
 
+fn generate_string_from_regex(rng: &mut impl rand::Rng,
+                              length: usize,
+                              randex: String,
+                              randstr: &mut String)
+{
+    // TODO: figure out what to do with bad unwraps
+    let gen = Regex::compile(randex.as_str(), length as u32).unwrap();
+    let samples = rng.sample_iter(&gen).take(1).collect::<Vec<String>>();
+    randstr.push_str(samples.get(0).unwrap());
+}
+
 fn main()
 {
     // Define argument/parameters
-    let mut arg_length = 8;
+    let mut arg_length_or_maxrepeat = 8;
     let mut opt_alphanum = false;
     let mut opt_printable = false;
     let mut opt_all = false;
     let mut opt_charscope = "".to_string();
+    let mut opt_regex = "".to_string();
     let mut opt_verbose = false;
 
     let mut rng = rand::thread_rng();
@@ -71,10 +86,14 @@ fn main()
     // Parse arguments
     {
         let mut parser = ArgumentParser::new();
-        parser.set_description("A handy tool to generate (pseudo-)random ASCII strings.");
-        parser.refer(&mut arg_length)
-            .add_argument("length", Store,
-            "The quantity of characters of which to generate.");
+        parser.set_description("A handy tool to generate (pseudo-)random strings.");
+        parser.refer(&mut arg_length_or_maxrepeat)
+            .add_argument("length | maxrepeat", Store,
+            "The quantity (length) of characters of which to generate. Or, the maximum \
+            amount of characters to repeat (maxrepeat) on unbounded regex quantifiers \
+            (e.g. `x*`, `x+` and `x{n,}`). If the regex expression is explicitly bounded, then \
+            this argument is ignored. (Tip: regex='a{4,}' maxrepeat=100 will generate a \
+            string between 4 and 104 characters long). (Default: 8)");
         parser.refer(&mut opt_all)
             .add_option(&["-A", "--all"], StoreTrue,
             "Generate from all ASCII characters.");
@@ -84,6 +103,11 @@ fn main()
         parser.refer(&mut opt_alphanum)
             .add_option(&["-N", "--alphanum"], StoreTrue,
             "Generate from only alpha-numeric ASCII characters.");
+        parser.refer(&mut opt_regex)
+            .add_option(&["-r", "--regex"], Store,
+            "A regex-like expression from which to generate random UTF-8 characters. An \
+            error is thrown if statement contains anchors (`^`, `$`, `\\A`, `\\z`) or word \
+            boundary assertions (`\\b`, `\\B`).");
         parser.refer(&mut opt_charscope)
             .add_option(&["-c", "--charscope"], Store,
             "A token of characters from which to randomly select.");
@@ -97,6 +121,7 @@ fn main()
     // NOTE: (This will make mode selecting A LOT easier than using
     // an if/else on combinations of boolean values.
     opmode = opmode | (((opt_charscope.len() > 0) as u8) * OP_CHARSCOPE);
+    opmode = opmode | (((opt_regex.len() > 0) as u8) * OP_REGEX);
     opmode = opmode | (opt_alphanum as u8 * OP_ALPHANUM);
     opmode = opmode | (opt_printable as u8 * OP_PRINTABLE);
     opmode = opmode | (opt_all as u8) * OP_ALLASCII;
@@ -104,21 +129,25 @@ fn main()
     // Print the input argument for debugging
     if opt_verbose
     {
-        eprintln!("debug: rchar length={}, alphanum={}, printable={}, all={}, charscope=\'{}\', \
-        verbose={}", arg_length, opt_alphanum, opt_printable, opt_all, opt_charscope, opt_verbose);
+        eprintln!("debug: rchar length|maxrepeat={}, alphanum={}, printable={}, all={}, \
+        charscope=\'{}\', regex=\'{}\', verbose={}", arg_length_or_maxrepeat, opt_alphanum,
+                  opt_printable, opt_all, opt_charscope, opt_regex, opt_verbose);
         eprintln!("debug: opmode={}", opmode);
     }
 
     // Allocate a buffer of the requested size
-    buf = String::with_capacity(arg_length);
+    buf = String::with_capacity(arg_length_or_maxrepeat);
 
     // Generate the random string based on the operation mode.
     match opmode
     {
-        OP_CHARSCOPE => generate_string_from_charscope(&mut rng, arg_length, opt_charscope,
-                                                       &mut buf),
+        OP_CHARSCOPE => generate_string_from_charscope(&mut rng, arg_length_or_maxrepeat,
+                                                       opt_charscope,&mut buf),
 
-        OP_ALPHANUM => generate_string_from_charscope(&mut rng, arg_length,
+        OP_REGEX => generate_string_from_regex(&mut rng, arg_length_or_maxrepeat,
+                                               opt_regex, &mut buf),
+
+        OP_ALPHANUM => generate_string_from_charscope(&mut rng, arg_length_or_maxrepeat,
                                                       format!("{}{}{}",
                                                               range2charscope(65,
                                                                               91),
@@ -128,15 +157,15 @@ fn main()
                                                                               58)),
                                                       &mut buf),
         // default option
-        OP_PRINTABLE | 0 => generate_string_from_charscope(&mut rng, arg_length,
-                                                       format!("{}{}",
+        OP_PRINTABLE | 0 => generate_string_from_charscope(&mut rng, arg_length_or_maxrepeat,
+                                                           format!("{}{}",
                                                                range2charscope(32,
                                                                                127),
                                                                range2charscope(128,
                                                                                256)),
-                                                       &mut buf),
+                                                           &mut buf),
 
-        OP_ALLASCII => generate_string_from_charscope(&mut rng, arg_length,
+        OP_ALLASCII => generate_string_from_charscope(&mut rng, arg_length_or_maxrepeat,
                                                       format!("{}",
                                                               range2charscope(0,
                                                                               256)),
